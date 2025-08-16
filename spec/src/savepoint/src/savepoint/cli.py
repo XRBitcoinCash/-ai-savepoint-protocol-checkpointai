4c998d7c0ff51cd5a84d6d963357ddf3677e1d97
Scott Anderton <XRbitcoincash@gmail.com>
2025-08-15 19:33:22 -0500

    Create cli.py

---
+++ spec/src/savepoint/src/savepoint/cli.py
@@ -0,0 +1,5 @@
+"""
+AI Save Point Protocol (UASPP) - Command Line Interface
+Usage examples:
+    python -m savepoint.cli init --app "MyApp" --app-version "0.1.0" --engine "gpt-4" --out savepoint
+  
